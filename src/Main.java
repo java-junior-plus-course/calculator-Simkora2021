38b365e0dda6a78423f2afa81a1b0bd4f4c59b4e
Simkora <simkora1996@gmail.com>
2020-11-20 22:30:58 +0300

    add Scanner

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,14 @@
-public class Main {
+import java.util.Scanner;
 
-    public static void main(String[] args) {
-	// write your code here
+public class Main {
+    public double self() {
+        Scanner sc = new Scanner(System.in);
+        double i = 0;
+        if (sc.hasNextDouble()) {
+            i = sc.nextDouble();
+        } else {
+            System.out.println(" neverno vvedeno ");
+        }
+        return i;
     }
 }
